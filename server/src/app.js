7592bd82c65cf1155297a9e4108bd120326cee4d
serhat-kose <serhatkse94@gmail.com>
2021-05-08 02:46:58 +0300

    sequelize define

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,9 @@ console.log("hello");
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const motgan = require('morgan');
+const morgan = require('morgan');
+const {sequelize} = require('./models');
+const config = require('./config/config');
 const morgan = require('morgan');
 
 
@@ -11,10 +13,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register',(req,res)=>{
-    res.send({
-        message: `Hello ${req.body.email}! Your user was register`
-    })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync().then(()=>{
+
+    app.listen(config.port)
+})
---
+++ server/src/config/config.js
@@ -0,0 +1,10 @@
+module.exports ={
+
+    port:8081,
+    db:{
+        database: process.env.DB_NAME || 'tabtracker',
+        user:cfgfgf,
+        password:dsfsfs,
+        options:cccbcbv
+    }
+}
