b6bc9fa6cc022af4985c6199ad6b0ffa0385639e
serhat-kose <serhatkse94@gmail.com>
2021-05-09 15:22:19 +0300

    validation registery with joi

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,18 @@
+const {User} = require('../models')
+
 module.exports = {
-   register (req,res){
-        res.send({
-            message: `Hello ${req.body.email}! Your user was register`
-        })
+    async register (req,res){
+
+        try {
+            const user = await User.create(req.body)
+
+            res.send(user.toJSON())
+        } catch (error) {
+            res.status(400).send({
+                error:'This Email already in use'
+            })
+        }
+ 
+       
     }
 }
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,40 @@
+const Joi = require('joi')
+
+module.exports = {
+    register(req,res,next){
+        
+        const schema = Joi.object({
+            email: Joi.string().email(),
+            password: Joi.string().regex(
+               new RegExp('^[a-zA-Z0-9]{8,32}$') 
+            )
+        }) 
+
+        const {error}= schema.validate(req.body);
+
+        if(error){
+
+            switch(error.details[0].context.key){
+                case 'email':
+                        res.status(400).send({
+                            error:'You must provide a valid email adress'
+                        })
+                break
+                case 'password':
+                        res.status(400).send({
+                            error:'You must provide a valid password'
+                        })
+                    break
+
+                    default:
+                        res.status(400).send({
+                            error:'Invalid registration information'
+                        })
+            }
+        }
+        else {
+            next()
+        }
+     
+    }
+}
