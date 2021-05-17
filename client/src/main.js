ec6eeaf6a681801700e992d82f3952d916cccb08
serhat-kose <serhatkse94@gmail.com>
2021-05-17 16:40:36 +0300

    edit

--- client/src/main.js
+++ client/src/main.js
@@ -9,12 +9,10 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-//merhaba
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-//hello guys
 
 Vue.config.productionTip = false
 
